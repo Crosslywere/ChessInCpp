3778ff0b798b82159e64ced8512a64c788ad7a47
Crosslywere <ogboruj@outlook.com>
2025-04-24 20:44:11 +0100

    Testing implemented classes

--- ChessInCpp/src/main.cpp
+++ ChessInCpp/src/main.cpp
@@ -1,4 +1,6 @@
 #include <Engine.h>
+#include <Framebuffer.h>
+#include <Timer.h>
 
 class Chess : public Engine {
 public:
@@ -10,11 +12,24 @@ public:
 	void onCreate() override {
 	}
 	void onUpdate() override {
+		Framebuffer::setClearColor(abs(sinf(Timer::getTotalTime())), abs(sinf(Timer::getTotalTime() + 0.5f)), 0);
 	}
 	void onRender() override {
+		Framebuffer::clearScreen();
+	}
+	void onResize() override {
+	}
+	void onExit() override {
 	}
 };
 
+#if defined (DEBUG) || !defined (_WIN32) // None windows systems and windows debug
 int main() {
 	Chess().play();
 }
+#else // Windows release
+#include <windows.h>
+int WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmd, int showCmd) {
+	Chess().play();
+}
+#endif
